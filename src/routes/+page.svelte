4f4521c4e973f7de26a05fa21dd932de898bc6de
ahitagnied <ad158@rice.edu>
2025-12-06 16:23:00 -0600

    -
    -
    
    -
    
    -

---
+++ src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<div class="page-container">
+	<h1>ABOUT</h1>
+
+	<p>
+		I am the founding engineer at <a href="https://optica.industries/"
+			>Optica Industries</a
+		>. Optica is building CV tools to solve the information assymetry problem
+		in the American Metal Industry. We are backed by Lightspeed and Neo. If you
+		are interested in joining,
+		<a href="https://calendly.com/ahitagnid/new-meeting">reach out</a>
+	</p>
+
+	<p>
+		I love ML and consumer AI exploring new ways to interact with reality &
+		imagination. I am a part of <a href="https://neo.com/">Neo</a> and scout
+		for <a href="https://www.clayvc.io/">Clay VC</a> where we invest as early
+		as possible, and were among the first investors of companies like
+		<a href="https://elevenlabs.io/">Eleven Labs</a> and
+		<a href="https://mistral.ai/">Mistral AI</a>
+	</p>
+
+	<p>
+		Previously, I was an ML Engineer at <a href="https://personainc.ai/"
+			>Persona AI</a
+		>, where I worked on Robot Learning and MLOps. Persona is building
+		humanoids to automate industrial workflows and currently has a deal with HD
+		Hundai. I also worked on Video Understanding and low-latency CV at
+		<a href="https://www.sievedata.com/">Sieve Data</a> powering video datasets for
+		Frontier AI Labs.
+	</p>
+
+	<p>
+		I am interested in novel methods to represent and interact with reality. My
+		interests include but are not limited to Neural Radiance Fields, World
+		Modelling, & JEPAs. I research Gaussian Splatting at the <a
+			href="https://computationalimaging.rice.edu/"
+			>Computational Imaging Group</a
+		>. Previously, I worked on Colloidal Robotics at the
+		<a
+			href="https://www.media.mit.edu/groups/nano-cybernetic-biotrek/overview/"
+			>MIT Media Lab</a
+		>, researched Li/Na-ion batteries at the
+		<a href="https://ajayan.rice.edu/">Ajayan Group</a>, and energy devices at
+		<a href="https://scholar.google.com/citations?user=JlmilbMAAAAJ&hl=en"
+			>IIT Guwahati</a
+		>.
+	</p>
+</div>
