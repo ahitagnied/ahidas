dcd35f23c3822d84d1765b146fbc66cdf1570bd8
ahitagnied <ad158@rice.edu>
2025-12-07 05:21:22 -0600

    ./research

---
+++ src/routes/research/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import ResearchItem from '$lib/components/ResearchItem.svelte';
+	import researchData from '$lib/data/research.yml?raw';
+	import yaml from 'js-yaml';
+
+	const researchItems = yaml.load(researchData);
+</script>
+
+<svelte:head>
+	<title>Ahitagni D | Research</title>
+</svelte:head>
+
+<div class="page-container">
+	<h1>Research</h1>
+
+	{#each researchItems as item}
+		<ResearchItem {item} />
+	{/each}
+</div>
+
