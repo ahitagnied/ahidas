03861b6fc4b89f0a534260e80f0b8af87e036153
ahitagnied <ad158@rice.edu>
2026-05-04 21:49:57 -0500

    simplify essay item to inline title and date

--- src/lib/components/EssayItem.svelte
+++ src/lib/components/EssayItem.svelte
@@ -2,23 +2,9 @@
 	let { item } = $props();
 </script>
 
-<div class="pb-3">
-	<a
-		href="/essays/{item.slug}"
-		class="flex items-start gap-3 no-underline group"
-	>
-		<img
-			src={item.image}
-			alt={item.title}
-			loading="lazy"
-			class="flex-shrink-0 aspect-square object-cover rounded"
-			style="height: calc(1em * 1.6 * 2);"
-		/>
-		<div class="flex-1 min-w-0">
-			<div class="font-[500] text-body">
-				{item.title}
-			</div>
-			<div class="text-muted">{item.date}</div>
-		</div>
+<div class="pb-[10px]">
+	<a href="/essays/{item.slug}" class="no-underline block group">
+		<span class="font-[500] text-body">{item.title}</span>
+		<span class="text-muted ml-2">{item.date}</span>
 	</a>
 </div>
