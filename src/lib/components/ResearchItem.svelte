4eaeeeccf80fe1043750ac65684fe90406f8f4b5
ahitagnied <ad158@rice.edu>
2026-06-19 17:50:41 -0700

    Add research images, update ResearchItem.svelte, research.yml, and layout.css

--- src/lib/components/ResearchItem.svelte
+++ src/lib/components/ResearchItem.svelte
@@ -6,41 +6,46 @@
 		authors?: string[];
 		journal: string;
 		year: number;
-		url?: string;
+		image?: string;
+		links?: Record<string, string>;
 	};
 
 	let { item }: { item: ResearchEntry } = $props();
 
-	const hasUrl = $derived.by(() => {
-		const url = item.url?.trim();
-		return Boolean(url && url !== '-');
-	});
+	const activeLinks = $derived(
+		Object.entries(item.links ?? {}).filter(([, url]) => url?.trim())
+	);
 </script>
 
-<div class="pb-3 font-[500]">
-	{#if hasUrl}
-		<a
-			href={item.url}
-			target="_blank"
-			rel="noopener noreferrer"
-			class="no-underline block group"
-		>
-			<div class="text-body">{item.title}</div>
-		</a>
-	{:else}
+<div class="flex items-center gap-4 pb-5 font-[500]">
+	{#if item.image}
+		<img
+			src={item.image}
+			alt={item.title}
+			class="w-[30%] shrink-0 aspect-[180/101] object-cover"
+		/>
+	{/if}
+	<div>
 		<div class="text-body">{item.title}</div>
-	{/if}
-	{#if item.authors?.length}
-		<div class="research-authors">
-			{#each item.authors as author, i}
-				{#if i > 0}<span>, </span>{/if}
-				{#if author === AUTHOR_SELF}
-					<span class="research-author-self">{author}</span>
-				{:else}
-					{author}
-				{/if}
-			{/each}
-		</div>
-	{/if}
-	<div class="research-meta">{item.journal}, {item.year}</div>
+		{#if item.authors?.length}
+			<div class="research-authors">
+				{#each item.authors as author, i}
+					{#if i > 0}<span>, </span>{/if}
+					{#if author === AUTHOR_SELF}
+						<span class="research-author-self">{author}</span>
+					{:else}
+						{author}
+					{/if}
+				{/each}
+			</div>
+		{/if}
+		<div class="research-meta">{item.journal}, {item.year}</div>
+		{#if activeLinks.length}
+			<div class="mt-1 flex gap-2">
+				{#each activeLinks as [label, url]}
+					<a href={url} target="_blank" rel="noopener noreferrer" class="research-link">{label}</a>
+				{/each}
+			</div>
+		{/if}
+	</div>
 </div>
