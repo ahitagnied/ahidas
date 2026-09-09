04d626ead1a00e9a1d453662cbd097be4b85a122
ahitagnied <ad158@rice.edu>
2026-09-09 05:09:51 +0200

    Add pic

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,8 +1,9 @@
-<section class="about site pt-[calc(2*var(--spacing-row))]">
+<section
+	class="about site grid gap-x-[24px] pt-[calc(2*var(--spacing-row))] wide:grid-cols-[7fr_3fr]"
+>
 	<div class="space-y-[18px] text-[13.5px] leading-[1.5]">
 		<p>
-			I work on Pretraining and WM+IDMs for 
-			Manipulation. I am advised by
+			I work on Pretraining and WM+IDMs for Manipulation. I am advised by
 			<a href="https://scholar.google.com/citations?user=wwf-WgUAAAAJ&hl=en">Vivek Boominathan</a>,
 			<a href="https://scholar.google.com/citations?user=S1x_xqcAAAAJ&hl=en">Randall Balestriero</a
 			>, &
@@ -17,17 +18,19 @@
 		</p>
 
 		<p>
-			Previously, I worked on VLAs at <a href="https://personainc.ai/">Persona AI</a>,
-			Multimodal Intelligence at <a href="https://www.sievedata.com/">Sieve</a>, 
-			& Energy, Nano Neuro-electronics at the
+			Previously, I worked on VLAs at <a href="https://personainc.ai/">Persona AI</a>, Multimodal
+			Intelligence at <a href="https://www.sievedata.com/">Sieve</a>, & Energy, Nano
+			Neuro-electronics at the
 			<a href="https://www.media.mit.edu/groups/nano-cybernetic-biotrek/overview/">MIT Media Lab</a
 			>,
 			<a href="https://scholar.google.com/citations?user=S5oLGEgAAAAJ&hl=en">Ajayan Group</a>, &
-			<a href="https://scholar.google.com/citations?user=JlmilbMAAAAJ&hl=en">IIT Guwahati</a>.
-			I am also on
+			<a href="https://scholar.google.com/citations?user=JlmilbMAAAAJ&hl=en">IIT Guwahati</a>. I am
+			also on
 			<a href="https://scholar.google.com/citations?user=GLTc7LcAAAAJ&hl=en">Google Scholar</a>,
 			<a href="https://github.com/ahitagnied">GitHub</a>, and
 			<a href="https://www.linkedin.com/in/ahitagnid/">LinkedIn</a>.
 		</p>
 	</div>
+
+	<img class="hidden h-fit w-full grayscale wide:block" src="/profile.jpg" alt="Ahitagni Das" />
 </section>
